067709e64860d8243d14c764e7a0b62aa9f04526
Mark-MDO47 <mdo@ieee.org>
2024-05-24 11:50:17 -0700

    mycopy.sh to copy sounds to D:

---
+++ resources/sounds/mycopy.sh
@@ -0,0 +1,28 @@
+cp ./0001_effect_c.wav D:/001.wav
+cp ./0002_effect_d.wav D:/002.wav
+cp ./0003_effect_e.wav D:/003.wav
+cp ./0004_effect_f.wav D:/004.wav
+cp ./0005_elect_c.wav D:/005.wav
+cp ./0006_elect_d.wav D:/006.wav
+cp ./0007_elect_e.wav D:/007.wav
+cp ./0008_elect_f.wav D:/008.wav
+cp ./0009_organ_c.wav D:/009.wav
+cp ./0010_organ_d.wav D:/010.wav
+cp ./0011_organ_e.wav D:/011.wav
+cp ./0012_organ_f.wav D:/012.wav
+cp ./0013_383205__spiceprogram__loading-sound_mdo47.wav D:/013.wav
+cp ./0014_397254__screamstudio__loading_mdo47.wav D:/014.wav
+cp ./0015_275537__wjoojoo__contact-mic-on-satellite-dish04_mdo47.wav D:/015.wav
+cp ./0016_388276__csnmedia__la-cucaracha-car-horn.wav D:/016.wav
+cp ./0017_118807__lmbubec__17-1985-ford-mustang-horn.wav D:/017.wav
+cp ./0018_352852__josepharaoh99__game-style-laser-beam_mdo47.wav D:/018.wav
+cp ./0019_7967__cfork__boing-raw_mdo47.wav D:/019.wav
+cp ./0020_91296__timbre__bwaang-2-reverb_mdo47.wav D:/020.wav
+cp ./0021_240297__jalastram__abstract-guitar-sfx-003_mdo47.wav D:/021.wav
+cp ./0022_170136__lazr2012__machinery-bo_mdo47.wav D:/022.wav
+cp ./0023_417131__cuddlenucks__science-fiction-noise-3_mdo47.wav D:/023.wav
+cp ./0024_introduction.wav D:/024.wav
+cp ./0025_silence.wav D:/025.wav
+ 
+mkdir D:/ATTRIBUTIONS
+cp  ./Attributions.html D:/ATTRIBUTIONS
